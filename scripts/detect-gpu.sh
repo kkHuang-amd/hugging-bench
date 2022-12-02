983e7116522e80966930b5cd1f030cac134eb83f
Jeremy Arnold <Jeremy.Arnold@amd.com>
2022-12-02 16:57:45 +0000

    Adjust batch size based on GPU architecture
    The batch sizes for BERT came from MLSE scripts.

---
+++ scripts/detect-gpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -f "/usr/bin/nvidia-smi" ]; then
+    echo "NVIDIA GPU detected"
+    gpu_vendor="NVIDIA"
+    gpu_architecture=$(nvidia-smi --query-gpu=name --format=csv,noheader | grep -m 1 -E -o ".{0,1}100"| xargs )
+elif [ -f "/opt/rocm/bin/rocm-smi" ]; then
+    echo "AMD GPU detected."
+    gpu_vendor="AMD"
+    gpu_architecture=$(rocminfo | grep -o -m 1 'gfx.*'| xargs )
+else
+    echo "Unable to detect GPU vendor"
+    exit 1
+fi
+
+MI200="gfx90a"
+MI100="gfx908"
+MI50="gfx906"
+A100="A100"
+V100="V100"
--- scripts/run-bert.sh
+++ scripts/run-bert.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+source $(dirname "${BASH_SOURCE[0]}")/detect-gpu.sh
+
+echo "GPU Vendor: ${gpu_vendor}"
+echo "GPU architecture: ${gpu_architecture}"
+
+case ${gpu_architecture} in
+    $MI200) batch_size=24; break;;
+    $MI100) batch_size=16; break;;
+    $MI50) batch_size=1; break;;
+    $A100) batch_size=8; break;;
+    $V100) batch_size=1; break;;
+    *) echo "Unrecognized GPU architecture: ${gpu_architecture}"; exit 1;;
+esac
+
+
 python3 /workspace/transformers/examples/pytorch/language-modeling/run_mlm.py \
     --model_name_or_path bert-large-uncased \
     --dataset_name wikitext \
@@ -8,6 +23,6 @@ python3 /workspace/transformers/examples/pytorch/language-modeling/run_mlm.py \
     --logging_steps 1 \
     --output_dir /tmp/test-mlm-bbu \
     --overwrite_output_dir \
-    --per_device_train_batch_size 24 \
+    --per_device_train_batch_size ${batch_size} \
     --fp16 \
     --skip_memory_metrics=True
