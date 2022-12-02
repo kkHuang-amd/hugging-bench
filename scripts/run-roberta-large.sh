35a7e0655273f0ec0a9d55f5d6027ee814573be4
Rebecca Lee <Rebecca.Lee@amd.com>
2022-12-02 21:45:02 +0000

    Init RoBERTa-Large run script

---
+++ scripts/run-roberta-large.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+source $(dirname "${BASH_SOURCE[0]}")/detect-gpu.sh
+
+echo "GPU Vendor: ${gpu_vendor}"
+echo "GPU architecture: ${gpu_architecture}"
+
+case ${gpu_architecture} in
+    $MI200) batch_size=24; break;;
+    $MI100) batch_size=24; break;;
+    $MI50) batch_size=2; break;;
+    $A100) batch_size=24; break;;
+    $V100) batch_size=2; break;;
+    *) echo "Unrecognized GPU architecture: ${gpu_architecture}"; exit 1;;
+esac
+
+python3 /workspace/transformers/examples/pytorch/question-answering/run_qa.py \
+       --model_name_or_path roberta-large \
+       --dataset_name squad \
+       --do_train \
+       --per_device_train_batch_size $batch_size \
+       --learning_rate 3e-5 \
+       --max_steps 150 \
+       --max_seq_length 384 \
+       --doc_stride 128 \
+       --output_dir /tmp/roberta_res \
+       --overwrite_output_dir \
+       --logging_steps 1 \
+       --fp16 \
+       --skip_memory_metrics=True \
+       "$@" \
+    2>&1 | tee log.txt
+
+# output performance metric
+performance=$(cat log.txt | grep -Eo "stable_train_samples_per_second':[^,]+" | sed "s/stable_train_samples_per_second': //g")
+
+echo "performance: $performance samples_per_second"
