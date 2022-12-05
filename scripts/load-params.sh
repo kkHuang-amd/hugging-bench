3c331365b90a9b2f06e52783d9a76aa578d6a439
Rebecca Lee <Rebecca.Lee@amd.com>
2022-12-05 06:43:32 +0000

    Init script for configurable parameters

---
+++ scripts/load-params.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Default parameters
+NGCD=8
+
+# User-defined parameters
+while [ ! -z "$1" ]; do
+    case $1 in
+        --n_gcd|-g)
+			shift
+			NGCD=$1
+			;;
+        *)
+        show_usage
+        exit 1
+        ;;
+	esac
+	shift
+done
