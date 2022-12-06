78127393f120e95ec9a7a57561c719e0133c5819
Rebecca Lee <Rebecca.Lee@amd.com>
2022-12-06 14:51:03 -0800

    Init load_execute_params for configurable parameters

---
+++ load_execute_params.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Default parameters
+NGCD=8
+HB_DOCKER_TAG=latest
+
+# User-defined parameters
+while [ ! -z "$1" ]; do
+    case $1 in
+        # Docker base image tag
+        --base_docker_tag|-bt)
+            shift
+            BASE_DOCKER_TAG=$1
+            ;;
+        # Hugging-Bench Docker image tag
+        --hb_docker_tag|-t)
+            shift
+            HB_DOCKER_TAG=$1
+            ;;
+        # Number of GCDs
+        --n_gcd|-g)
+            shift
+            NGCD=$1
+            ;;
+        *)
+        show_usage
+        exit 1
+        ;;
+    esac
+    shift
+done
